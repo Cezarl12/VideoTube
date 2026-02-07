2478c6bcda89e7e4689013afa80c6adddc5a93a4
Cezarl12 <cezar.lauran15@gmail.com>
2026-02-07 16:23:04 +0200

    healthCheck

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import healthCheckRouter from "./routes/healthcheck.route.js";
 
 const app = express();
 app.use(
@@ -13,4 +14,7 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
 export { app };
